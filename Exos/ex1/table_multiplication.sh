96da81e7207762e99c12b2d7fe204d6e31a2d4cc
Thomas-cbs <thomas.combes@e.rascol.net>
2026-09-25 11:54:56 +0200

    exo 1 fini 2 entamé

--- Exos/ex1/table_multiplication.sh
+++ Exos/ex1/table_multiplication.sh
@@ -8,10 +8,22 @@
 ################################################################################
 
 # TODO: Demander un nombre à l'utilisateur
-
+#echo "saissisez un entier valide."
+read nb
 
 # TODO: Valider que l'entrée est bien un nombre
+if [[ "$nb" =~ ^-?[0-9]+$ ]]; then
+    #echo "C'est un nombre entier valide."
+    
+    for ((i=1; i<11; i++))
+    do
+        result=$(($nb*$i))
+        echo "$nb x $i = $result"
+    done
 
+else
+    echo "Ce n'est pas un entier valide."
+fi
 
 # TODO: Afficher la table de multiplication de 1 à 10
 
--- Exos/ex2/devine_nombre.sh
+++ Exos/ex2/devine_nombre.sh
@@ -9,10 +9,40 @@
 ################################################################################
 
 # TODO: Vérifier que 2 paramètres sont fournis
-
-
+if [ "$#" -ne 2 ]; then
+    echo "Erreur : Vous devez fournir exactement 2 paramètres."
+    echo "Usage: $0 <paramètre1> <paramètre2>"
+    exit 1
+fi
+$essais=0
+$nb_aleatoire=0
+$essais_restant=0
+$test=0
 # TODO: Valider que les paramètres sont des nombres
+if [[ "$1" =~ ^[0-9]+$ ]]; then
+    
+    if [ $1 -lt $2 ]; then
+        nb_aleatoire=$(($1 + RANDOM % $2))
 
+
+        i=0 
+        until [ $i -gt 5 ]; do 
+            essais_restant=$((5-$1))
+            echo "essayer de deviner le nombre entre $1 et $2 , il vous reste $i essaie(s) :" 
+            i=$(($i+1))
+            read test
+            if [ $1 -lt $2 ]; do
+
+        done 
+
+
+    else
+        echo "le premier paramétre doit etre un nombre stictement plue petit que le deuxiéme"
+    fi
+
+else
+    echo "saissisez un entier valide."
+fi
 
 # TODO: Valider que min < max
 
